110ab8a4a01837204fa862fd3e1ca0c4027b992e
James Zhao <xz2987@columbia.edu>
2020-11-11 10:09:12 -0500

    jj

--- server.py
+++ server.py
@@ -166,7 +166,7 @@ def another():
 @app.route('/add', methods=['POST'])
 def add():
   name = request.form['name']
-  g.conn.execute('INSERT INTO test(name) VALUES (%s)', name)
+  g.conn.execute('INSERT INTO students(sid) VALUES (%s)', name)
   return redirect('/')
 
 
